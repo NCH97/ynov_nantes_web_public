5c95c1d52df348ca68fb55bb9e9edb0fa07a3b78
NCH97 <Blingyb.chnn@gmaul.com>
2022-11-25 11:45:09 +0100

    init commit

---
+++ src/components/Asval/Asval.js
@@ -0,0 +1,13 @@
+import { Button } from 'bootstrap';
+import react from 'react';
+import './Asval.css'
+
+const Asval = () => {
+    return (
+        <button>
+            ASVAL
+        </button>
+    );
+}
+
+export default Asval;
